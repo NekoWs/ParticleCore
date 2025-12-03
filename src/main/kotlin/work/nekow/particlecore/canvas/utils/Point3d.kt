7cd37e18521a4460c476e2ed0579f5f734759ad1
NekoW <1689295608@qq.com>
2025-12-03 21:50:16 +0800

    重命名

--- src/main/kotlin/work/nekow/particlecore/canvas/utils/Point3d.kt
+++ src/main/kotlin/work/nekow/particlecore/canvas/utils/Point3d.kt
@@ -5,53 +5,53 @@ import kotlin.math.pow
 import kotlin.math.sqrt
 
 @Suppress("unused")
-data class Point3D(
+data class Point3d(
     val x: Double,
     val y: Double,
     val z: Double,
 ) {
     companion object {
-        val ZERO = Point3D(0.0, 0.0, 0.0)
+        val ZERO = Point3d(0.0, 0.0, 0.0)
     }
     constructor(vec3d: Vec3d) : this(vec3d.x, vec3d.y, vec3d.z)
 
-    operator fun plus(p: Point3D) =
-        Point3D(x + p.x, y + p.y, z + p.z)
+    operator fun plus(p: Point3d) =
+        Point3d(x + p.x, y + p.y, z + p.z)
 
-    fun plus(x: Double, y: Double, z: Double) = Point3D(x + x, y + y, z + z)
+    fun plus(x: Double, y: Double, z: Double) = Point3d(x + x, y + y, z + z)
 
-    operator fun minus(p: Point3D) =
-        Point3D(x - p.x, y - p.y, z - p.z)
+    operator fun minus(p: Point3d) =
+        Point3d(x - p.x, y - p.y, z - p.z)
 
-    operator fun times(p: Point3D) =
-        Point3D(x * p.x, y * p.y, z * p.z)
+    operator fun times(p: Point3d) =
+        Point3d(x * p.x, y * p.y, z * p.z)
 
-    operator fun div(p: Point3D) =
-        Point3D(x / p.x, y / p.y, z / p.z)
+    operator fun div(p: Point3d) =
+        Point3d(x / p.x, y / p.y, z / p.z)
 
     operator fun plus(other: Double) =
-        Point3D(x + other, y + other, z + other)
+        Point3d(x + other, y + other, z + other)
 
     operator fun minus(other: Double) =
-        Point3D(x - other, y - other, z - other)
+        Point3d(x - other, y - other, z - other)
 
     operator fun times(scalar: Double) =
-        Point3D(x * scalar, y * scalar, z * scalar)
+        Point3d(x * scalar, y * scalar, z * scalar)
 
     operator fun div(scalar: Double) =
-        Point3D(x / scalar, y / scalar, z / scalar)
+        Point3d(x / scalar, y / scalar, z / scalar)
 
-    fun dot(other: Point3D): Double =
+    fun dot(other: Point3d): Double =
         x * other.x + y * other.y + z * other.z
 
-    fun distanceTo(other: Point3D) = sqrt(
+    fun distanceTo(other: Point3d) = sqrt(
         (x - other.x).pow(2) +
                 (y - other.y).pow(2) +
                 (z - other.z).pow(2)
     )
 
-    fun cross(other: Point3D): Point3D =
-        Point3D(
+    fun cross(other: Point3d): Point3d =
+        Point3d(
             y * other.z - z * other.y,
             z * other.x - x * other.z,
             x * other.y - y * other.x
@@ -62,7 +62,7 @@ data class Point3D(
 
     fun magnitude(): Double = sqrt(lengthSquared())
 
-    fun normalize(): Point3D {
+    fun normalize(): Point3d {
         val len = magnitude()
         return if (len > 0) this / len else this
     }
--- src/main/kotlin/work/nekow/particlecore/canvas/utils/Rotation.kt
+++ src/main/kotlin/work/nekow/particlecore/canvas/utils/Rotation.kt
@@ -1,3 +1,3 @@
 package work.nekow.particlecore.canvas.utils
 
-data class Rotation(val axis: Point3D, val angle: Double)
+data class Rotation(val axis: Point3d, val angle: Double)
