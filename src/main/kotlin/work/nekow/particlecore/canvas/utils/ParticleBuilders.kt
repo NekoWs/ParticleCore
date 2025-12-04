383063caade75af45654fac688ced29d1f84352a
NekoW <1689295608@qq.com>
2025-12-05 00:43:13 +0800

    增加参数

--- src/main/kotlin/work/nekow/particlecore/canvas/utils/ParticleBuilders.kt
+++ src/main/kotlin/work/nekow/particlecore/canvas/utils/ParticleBuilders.kt
@@ -12,12 +12,17 @@ data class ParticleBuilders(
     /**
      * 批量移动并召唤粒子效果
      */
-    fun spawnAt(world: ServerWorld, pos: Vec3d, delay: Int = 0) {
+    fun spawnAt(
+        world: ServerWorld,
+        pos: Vec3d,
+        delay: Int = 0,
+        particleDelay: Double = 0.0
+    ) {
         val move = particles.map { particle ->
             particle.clone().pos(particle.pos.add(pos))
         }
         ParticleUtils.spawnParticles(
-            world, move, delay
+            world, move, delay, particleDelay
         )
     }
 }
