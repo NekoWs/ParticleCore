856263c4f01a34ccddcdbceaf7338419d461f261
NekoW <1689295608@qq.com>
2025-12-04 12:06:21 +0800

    改成 StartClientTick 但好像没什么用

--- src/client/kotlin/work/nekow/particlecore/client/ParticlecoreClient.kt
+++ src/client/kotlin/work/nekow/particlecore/client/ParticlecoreClient.kt
@@ -4,23 +4,9 @@ import net.fabricmc.api.ClientModInitializer
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking
 import net.minecraft.client.MinecraftClient
-import work.nekow.particlecore.network.PacketFourierParticleS2C
-import work.nekow.particlecore.network.PacketFunctionParticlesS2C
-import work.nekow.particlecore.network.PacketLineParticlesS2C
-import work.nekow.particlecore.network.PacketMarkDeadS2C
-import work.nekow.particlecore.network.PacketMoveParticleS2C
-import work.nekow.particlecore.network.PacketParticleS2C
-import work.nekow.particlecore.network.PacketRemoveTickParticlesS2C
-import work.nekow.particlecore.network.PacketVelocityParticleS2C
-import work.nekow.particlecore.client.listeners.FourierParticleHandler
-import work.nekow.particlecore.client.listeners.FunctionParticlesHandler
-import work.nekow.particlecore.client.listeners.LineParticleHandler
-import work.nekow.particlecore.client.listeners.MarkDeadHandler
-import work.nekow.particlecore.client.listeners.MoveParticleHandler
-import work.nekow.particlecore.client.listeners.PacketParticleHandler
-import work.nekow.particlecore.client.listeners.RemoveTickParticlesHandler
-import work.nekow.particlecore.client.listeners.VelocityParticleHandler
+import work.nekow.particlecore.client.listeners.*
 import work.nekow.particlecore.client.particle.ParticleManager
+import work.nekow.particlecore.network.*
 
 class ParticlecoreClient : ClientModInitializer {
     companion object {
@@ -54,10 +40,10 @@ class ParticlecoreClient : ClientModInitializer {
             FunctionParticlesHandler()
         )
 
-        ClientTickEvents.END_CLIENT_TICK.register { client ->
+        ClientTickEvents.START_CLIENT_TICK.register { client ->
             ParticleManager.tick(client)
         }
-        ClientTickEvents.END_WORLD_TICK.register { world ->
+        ClientTickEvents.START_WORLD_TICK.register { world ->
             ParticleManager.worldTick(world)
         }
     }
