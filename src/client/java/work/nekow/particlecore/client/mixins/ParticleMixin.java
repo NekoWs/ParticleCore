25e9820c511a9d5685722db08eac7d63acfe0259
NekoW <1689295608@qq.com>
2025-12-18 21:42:41 +0800

    移除未使用的 import

--- src/client/java/work/nekow/particlecore/client/mixins/ParticleMixin.java
+++ src/client/java/work/nekow/particlecore/client/mixins/ParticleMixin.java
@@ -8,7 +8,6 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import work.nekow.particlecore.client.particle.ParticleManager;
 import work.nekow.particlecore.utils.FinalValues;
